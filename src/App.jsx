382874851c6c1216158c79a0c301a52b01918f62
HaiDang <haidangnguyen19101990@gmail.com>
2025-12-09 23:31:00 +0700

    Fix disapear token error

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React, { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import AddCar from "./components/AddCar/AddCar";
 import ManageCar from "./components/ManageCar/ManageCar";
@@ -9,7 +10,11 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 function App() {
-  const { isLoggedIn } = useAuth();
+  const { isLoggedIn, refreshSession } = useAuth();
+  useEffect(() => {
+    refreshSession();
+  }, [refreshSession]);
+
   if (!isLoggedIn) {
     return (
       <>
